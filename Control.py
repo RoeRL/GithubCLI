b117068981e0f6afd05ee2649d6e5599321e5b14
RoeRL <bimo.17168@gmail.com>
2025-12-23 21:14:01 +0700

    [RR]: Add repository function

--- Control.py
+++ Control.py
@@ -6,4 +6,11 @@ from dotenv import load_dotenv, dotenv_values
 load_dotenv() 
 
 url = "https://api.github.com/user/repos"
-token = os.getenv(PAT_KEY)
+token = os.getenv("PAT_KEY")
+
+headers = {"Authorization" : "token {}".format(token)}
+RepositoryName = input("Enter your repository name: ")
+print("Making "+RepositoryName)
+
+data = {"name" : "{}".format(RepositoryName)}
+requests.post(url, data = json.dumps(data), headers=headers)
