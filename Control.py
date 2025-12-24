4961a0e56c3a72870fda887fb0d8bd350edb6755
RoeRL <bimo.17168@gmail.com>
2025-12-24 11:17:31 +0700

    [RR]: add function for create repo

--- Control.py
+++ Control.py
@@ -1,16 +1,14 @@
 import requests
 import os
-import json
+import json 
 
-from dotenv import load_dotenv, dotenv_values 
-load_dotenv() 
+def createRepo(PAT_KEY):
+    url = "https://api.github.com/user/repos"
+    token = PAT_KEY
 
-url = "https://api.github.com/user/repos"
-token = os.getenv("PAT_KEY")
+    headers = {"Authorization" : "token {}".format(token)}
+    RepositoryName = input("Enter your repository name: ")
+    print("Making "+RepositoryName)
 
-headers = {"Authorization" : "token {}".format(token)}
-RepositoryName = input("Enter your repository name: ")
-print("Making "+RepositoryName)
-
-data = {"name" : "{}".format(RepositoryName)}
-requests.post(url, data = json.dumps(data), headers=headers)
+    data = {"name" : "{}".format(RepositoryName)}
+    requests.post(url, data = json.dumps(data), headers=headers)
