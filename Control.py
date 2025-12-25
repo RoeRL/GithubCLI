ec1e1d2436e66bfa0183c4f82809103d352038bd
RoeRL <bimo.17168@gmail.com>
2025-12-26 06:28:15 +0700

    [RR]: Add issue create function

--- Control.py
+++ Control.py
@@ -4,26 +4,38 @@ import json
 
 class GitCLI():
     private_key = "0"
+    username
 
 
     def createRepo():
-        url = "https://api.github.com/user/repos"
-        token = __private_key
+        
+        __token = private_key
         try:
-            headers = {"Authorization" : "token {}".format(token)}
+            headers = {"Authorization" : "token {}".format(__token)}
             RepositoryName = input("Enter your repository name: ")
             print("Making "+RepositoryName)
+            __url = "https://api.github.com/user/repos"
 
             data = {"name" : "{}".format(RepositoryName)}
             requests.post(url, data = json.dumps(data), headers=headers)
         except Exception as e: print(e)
 
-    def deleteRepo(Username, RepoName):
-        token = __private_key
+    def deleteRepo(RepoName):
+        __token = private_key
         try:
-            headers = {"Authorization" : "token {}".format(m_patkey)}
-            username = Username
-            RepositoryName = RepoName
-            url = url = "https://api.github.com/user/repos/{}/{}".format(username, RepositoryName)
+            headers = {"Authorization" : "token {}".format(__token)}
+            __RepositoryName = RepoName
+            __url = "https://api.github.com/user/repos/{}/{}".format(username, __RepositoryName)
             requests.delete(url, headers=headers)
+        except Exception as e: print(e)
+
+    def createIssue(RepoName):
+        __token = private_key
+        data = {}
+        try:
+            __headers = {"Authprization" : "token {}".format(__token)}
+            __RepositoryName = RepoName
+            __url= "https://api.github.com/repos/{}/{}/issues".format(username, __RepoName)
+            requests.post(url, data=json.dumps(data), headers=headers)
+
         except Exception as e: print(e)
