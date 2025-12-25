95ea2c7edba79a85c9012476273c356d8a8567d5
RoeRL <bimo.17168@gmail.com>
2025-12-25 21:04:18 +0700

    [RR]: Adding class to function

--- Control.py
+++ Control.py
@@ -2,20 +2,25 @@ import requests
 import os
 import json 
 
-def createRepo(PAT_KEY):
-    url = "https://api.github.com/user/repos"
-    token = PAT_KEY
+class GitCLI():
+    private_key = "0"
 
-    headers = {"Authorization" : "token {}".format(token)}
-    RepositoryName = input("Enter your repository name: ")
-    print("Making "+RepositoryName)
 
-    data = {"name" : "{}".format(RepositoryName)}
-    requests.post(url, data = json.dumps(data), headers=headers)
+    def createRepo():
+        url = "https://api.github.com/user/repos"
+        token = __private_key
 
-def deleteRepo(Username, RepoName):
-    headers = {"Authorization" : "token {}".format(token)}
-    username = Username
-    RepositoryName = RepoName
-    url = url = "https://api.github.com/user/repos/{}/{}".format(username, RepositoryName)
-    requests.delete(url, headers=headers)
+        headers = {"Authorization" : "token {}".format(token)}
+        RepositoryName = input("Enter your repository name: ")
+        print("Making "+RepositoryName)
+
+        data = {"name" : "{}".format(RepositoryName)}
+        requests.post(url, data = json.dumps(data), headers=headers)
+
+    def deleteRepo(Username, RepoName):
+        token = __private_key
+        headers = {"Authorization" : "token {}".format(m_patkey)}
+        username = Username
+        RepositoryName = RepoName
+        url = url = "https://api.github.com/user/repos/{}/{}".format(username, RepositoryName)
+        requests.delete(url, headers=headers)
