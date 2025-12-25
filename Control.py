ede6d3a2267b5a8619cccc438dc48e2c0d749a0d
RoeRL <bimo.17168@gmail.com>
2025-12-26 06:02:23 +0700

    [RR]: Adding error detail on function

--- Control.py
+++ Control.py
@@ -9,18 +9,21 @@ class GitCLI():
     def createRepo():
         url = "https://api.github.com/user/repos"
         token = __private_key
+        try:
+            headers = {"Authorization" : "token {}".format(token)}
+            RepositoryName = input("Enter your repository name: ")
+            print("Making "+RepositoryName)
 
-        headers = {"Authorization" : "token {}".format(token)}
-        RepositoryName = input("Enter your repository name: ")
-        print("Making "+RepositoryName)
-
-        data = {"name" : "{}".format(RepositoryName)}
-        requests.post(url, data = json.dumps(data), headers=headers)
+            data = {"name" : "{}".format(RepositoryName)}
+            requests.post(url, data = json.dumps(data), headers=headers)
+        except Exception as e: print(e)
 
     def deleteRepo(Username, RepoName):
         token = __private_key
-        headers = {"Authorization" : "token {}".format(m_patkey)}
-        username = Username
-        RepositoryName = RepoName
-        url = url = "https://api.github.com/user/repos/{}/{}".format(username, RepositoryName)
-        requests.delete(url, headers=headers)
+        try:
+            headers = {"Authorization" : "token {}".format(m_patkey)}
+            username = Username
+            RepositoryName = RepoName
+            url = url = "https://api.github.com/user/repos/{}/{}".format(username, RepositoryName)
+            requests.delete(url, headers=headers)
+        except Exception as e: print(e)
