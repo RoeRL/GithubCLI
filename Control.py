963007ffab28a7dc879afc6d93162c1d26a3c419
RoeRL <bimo.17168@gmail.com>
2025-12-26 18:11:35 +0700

    [RR]: Adding issue commenting

--- Control.py
+++ Control.py
@@ -4,7 +4,7 @@ import json
 
 class GitCLI():
     private_key = "0"
-    username
+    username = "Unknown"
 
 
     def createRepo():
@@ -17,25 +17,40 @@ class GitCLI():
             __url = "https://api.github.com/user/repos"
 
             __data = {"name" : "{}".format(RepositoryName)}
-            requests.post(url, data = json.dumps(data), headers=headers)
+            requests.post(__url, data = json.dumps(__data), headers=__headers)
         except Exception as e: print(e)
 
     def deleteRepo(RepoName):
-        __token = private_key
+        __token = self.private_key
         try:
             __headers = {"Authorization" : "token {}".format(__token)}
             __RepositoryName = RepoName
             __url = "https://api.github.com/user/repos/{}/{}".format(username, __RepositoryName)
-            requests.delete(url, headers=headers)
+            requests.delete(__url, headers=__headers)
         except Exception as e: print(e)
 
-    def createIssue(RepoName):
+    def issuesCreate(RepoName):
         __token = private_key
-        data = {}
+        title = "No Title"
+        __data = {
+            "title" : title
+        }
         try:
             __headers = {"Authprization" : "token {}".format(__token)}
             __RepositoryName = RepoName
             __url= "https://api.github.com/repos/{}/{}/issues".format(username, __RepoName)
-            requests.post(url, data=json.dumps(data), headers=headers)
+            requests.post(__url, data=json.dumps(__data), headers=__headers)
 
         except Exception as e: print(e)
+
+    def issuesComment(RepoName, issueNumber):
+        __RepositoryName = RepoName
+        __headers = {
+            "Authorization" : "token {}".format(__token)
+        }
+        comment = "No Comment"
+        __data = {
+            "body" : comment
+        }
+        __url = "https://api.github.com/repos/{}/{}/issues/{}/comments".format(username, __RepositoryName, issueNumber)
+        requests.post(__url,data=json.dumps(__data),headers=__headers)
