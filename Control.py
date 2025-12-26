8a6d3911e116d58ae97705734f8194bded6a254e
RoeRL <bimo.17168@gmail.com>
2025-12-26 17:32:27 +0700

    [RR]: Add access modifier on GitCLI Class

--- Control.py
+++ Control.py
@@ -11,19 +11,19 @@ class GitCLI():
         
         __token = private_key
         try:
-            headers = {"Authorization" : "token {}".format(__token)}
-            RepositoryName = input("Enter your repository name: ")
+            __headers = {"Authorization" : "token {}".format(__token)}
+            __RepositoryName = input("Enter your repository name: ")
             print("Making "+RepositoryName)
             __url = "https://api.github.com/user/repos"
 
-            data = {"name" : "{}".format(RepositoryName)}
+            __data = {"name" : "{}".format(RepositoryName)}
             requests.post(url, data = json.dumps(data), headers=headers)
         except Exception as e: print(e)
 
     def deleteRepo(RepoName):
         __token = private_key
         try:
-            headers = {"Authorization" : "token {}".format(__token)}
+            __headers = {"Authorization" : "token {}".format(__token)}
             __RepositoryName = RepoName
             __url = "https://api.github.com/user/repos/{}/{}".format(username, __RepositoryName)
             requests.delete(url, headers=headers)
