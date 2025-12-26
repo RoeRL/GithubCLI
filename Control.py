08f135fe0d4212ef007430e7dac3cf2b01e26be7
RoeRL <bimo.17168@gmail.com>
2025-12-26 21:35:02 +0700

    [RR]:Optimize Control

--- Control.py
+++ Control.py
@@ -3,54 +3,50 @@ import os
 import json 
 
 class GitCLI():
-    private_key = "0"
-    username = "Unknown"
+    def __init__(self, private_key, username):
+        self.private_key = private_key
+        self.username = username
+        self.headers = {"Authorization" : "token {}".format(private_key)}
 
-
-    def createRepo():
-        
-        __token = private_key
+    def createRepo(self):
         try:
-            __headers = {"Authorization" : "token {}".format(__token)}
             __RepositoryName = input("Enter your repository name: ")
             print("Making "+RepositoryName)
             __url = "https://api.github.com/user/repos"
 
             __data = {"name" : "{}".format(RepositoryName)}
-            requests.post(__url, data = json.dumps(__data), headers=__headers)
+            requests.post(__url, data = json.dumps(__data), headers=self.headers)
         except Exception as e: print(e)
 
-    def deleteRepo(RepoName):
-        __token = self.private_key
+    def deleteRepo(self, RepoName):
         try:
-            __headers = {"Authorization" : "token {}".format(__token)}
+            __headers = {"Authorization" : "token {}".format(self.private_key)}
             __RepositoryName = RepoName
-            __url = "https://api.github.com/user/repos/{}/{}".format(username, __RepositoryName)
-            requests.delete(__url, headers=__headers)
+            __url = "https://api.github.com/user/repos/{}/{}".format(self.username, __RepositoryName)
+            requests.delete(__url, headers=self.headers)
         except Exception as e: print(e)
 
-    def issuesCreate(RepoName):
-        __token = private_key
+    def issuesCreate(self, RepoName):
         title = "No Title"
         __data = {
             "title" : title
         }
         try:
-            __headers = {"Authprization" : "token {}".format(__token)}
+            __headers = {"Authprization" : "token {}".format(self.private_key)}
             __RepositoryName = RepoName
-            __url= "https://api.github.com/repos/{}/{}/issues".format(username, __RepoName)
-            requests.post(__url, data=json.dumps(__data), headers=__headers)
+            __url= "https://api.github.com/repos/{}/{}/issues".format(self.username, __RepoName)
+            requests.post(__url, data=json.dumps(__data), headers=self.headers)
 
         except Exception as e: print(e)
 
-    def issuesComment(RepoName, issueNumber):
+    def issuesComment(self, RepoName, issueNumber):
         __RepositoryName = RepoName
         __headers = {
-            "Authorization" : "token {}".format(__token)
+            "Authorization" : "token {}".format(self.private_key)
         }
         comment = "No Comment"
         __data = {
             "body" : comment
         }
-        __url = "https://api.github.com/repos/{}/{}/issues/{}/comments".format(username, __RepositoryName, issueNumber)
-        requests.post(__url,data=json.dumps(__data),headers=__headers)
+        __url = "https://api.github.com/repos/{}/{}/issues/{}/comments".format(self.username, __RepositoryName, issueNumber)
+        requests.post(__url,data=json.dumps(__data),headers=self.headers)
